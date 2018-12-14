22d83158337dc5b77a41d515c2c4c499cc0af74a
jack jin <superjwl@gmail.com>
2018-12-14 16:04:59 +0800

    - fix weather api, but still cannot work

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -66,7 +66,7 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     private void requestWeather(String weatherId) {
-        String weatherUrl = "http://guolin.tech/api/weather?cityid='" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
